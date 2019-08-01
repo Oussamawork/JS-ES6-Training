30f8b68f827ea33c87ed37ac9ee0b06211c79ef8
Oussamawork <37862670+Oussamawork@users.noreply.github.com>
2019-08-01 21:50:56 +0200

    Analog-Clock

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-const con = document.querySelector("#taille12");
-
-if(con.hasAttribute("_blank")) {
-    console.log(con.getAttribute("_blank"));
-} else {
-    console.log(con.setAttribute("_blank","_blank"));
-}
-
-console.log("Done.");
-
-console.log(con.attributes);
-
-///// 
-
-
-const featured = document.querySelector(".featured-image");
-const theimg = featured.querySelector("img");
-
-var altText = theimg.getAttribute("alt"); 
-
-var CaptionElement = document.createElement("featuredFigure");
-
-var CaptionText = document.createTextNode(altText);
-
-CaptionElement.appendChild(CaptionText);
-featured.appendChild(CaptionElement);
-
-theimg.toggleAttribute("alt");
-
-console.log(featured);
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
